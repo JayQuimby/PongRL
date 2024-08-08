83ec2526c56921e49f85b319395a0dfe8bdf0090
Jack <jack.quimby54@gmail.com>
2024-08-07 21:39:30 -0400

    DNQ working

--- agent.py
+++ agent.py
@@ -1,42 +1,38 @@
 import numpy as np
 from collections import deque
 import random
-
-# Hyperparameters
-ACTION_SIZE = 5
-GAMMA = 0.95
-LEARNING_RATE = 0.001
-MEMORY_SIZE = 2000
-BATCH_SIZE = 32
-EPSILON_DECAY = 0.995
-MIN_EPSILON = 0.01
+import os
+from global_vars import *
 
 class PongAgent:
-    def __init__(self, input_size):
+    def __init__(self):
         self.memory = deque(maxlen=MEMORY_SIZE)
         self.epsilon = 1.0
-        self.model = self._build_model(input_size)
+        self.model = self._build_model()
 
-    def _build_model(self, inp_dim):
+    def _build_model(self):
         import tensorflow as tf
         from tensorflow.keras.models import Sequential
         from tensorflow.keras.layers import Dense
-
-        model = Sequential()
-        model.add(Dense(24, input_dim=inp_dim, activation='relu'))
-        model.add(Dense(24, activation='relu'))
-        model.add(Dense(ACTION_SIZE, activation='sigmoid'))
-        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE))
-        return model
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        if os.path.exists(MODEL_PATH):
+            return self.load(MODEL_PATH)
+        else:
+            model = Sequential()
+            model.add(Dense(24, input_dim=INPUT_SIZE, activation='relu'))
+            model.add(Dense(24, activation='relu'))
+            model.add(Dense(ACTION_SIZE, activation='sigmoid'))
+            model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE))
+            return model
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return -1 #random.randrange(ACTION_SIZE)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])
+        act_values = self.model.predict(state)[0]
+        return np.where(act_values > 0.5, 1, 0)
 
     def replay(self):
         if len(self.memory) < BATCH_SIZE:
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 from global_vars import *
 from objects import Ball, Paddle, Obstacle
 from agent import PongAgent
@@ -7,6 +8,7 @@ p_conf = load_conf('paddle')
 b_conf = load_conf('ball')
 o_conf = load_conf('obstacle')
 rr = lambda x: round(x, 1)
+verify = lambda x: x['p1'] < MAX_SCORE & x['p2'] < MAX_SCORE
 
 class GameDisplay:
     def __init__(self, game):
@@ -32,8 +34,8 @@ class GameDisplay:
         variables = [
             (f'Ball Position: ({self.game.ball.x // 10 * 10}, {self.game.ball.y // 10 * 10})', self.font, (MID_WIDTH + 50, SCREEN_HEIGHT + 10)),
             (f'Ball Velocity: ({rr(self.game.ball.vx)}, {rr(self.game.ball.vy)})', self.font, (MID_WIDTH - 210, SCREEN_HEIGHT + 10)),
-            (f'Left Paddle Y: {rr(self.game.left_paddle.y)}', self.font, (10, SCREEN_HEIGHT + 10)),
-            (f'Right Paddle Y: {rr(self.game.right_paddle.y)}', self.font, (SCREEN_WIDTH - 210, SCREEN_HEIGHT + 10)),
+            (f'Left Paddle Y: {rr(self.game.paddles[0].y)}', self.font, (10, SCREEN_HEIGHT + 10)),
+            (f'Right Paddle Y: {rr(self.game.paddles[1].y)}', self.font, (SCREEN_WIDTH - 210, SCREEN_HEIGHT + 10)),
             (f'P1: {score["p1"]} | P2: {score["p2"]}', self.big_font, (MID_WIDTH - 70, SCREEN_HEIGHT + 55)),
             (f'fps: {self.fps}', self.small_font, (0, 0), (255, 255, 0)),
             (f'P1 Reward: {rr(rewards[0])}', self.font, (20, SCREEN_HEIGHT + 55)),
@@ -58,8 +60,8 @@ class GameDisplay:
             pygame.draw.rect(self.screen, (255,255,255), (left_mid, i, mid_range, 1))
 
         self.game.ball.draw(self.screen)
-        self.game.left_paddle.draw(self.screen)
-        self.game.right_paddle.draw(self.screen)
+        self.game.paddles[0].draw(self.screen)
+        self.game.paddles[1].draw(self.screen)
         for obstacle in self.game.obstacles:
             obstacle.draw(self.screen)
 
@@ -78,43 +80,49 @@ class GameDisplay:
 class Game:
     def __init__(self, **config):
         pygame.init()
-        self.config = config
+        self.players = config.get('players', 0)
+        self.nn = config.get('nn', 0)
+        self.train = config.get('training', False)
+        self.games = config.get('num_games', 1)
+        
         self.read_out = GameDisplay(self)
-
-        self.nn_model = None
-        if config.get('nn', False):
-            self.nn_model = PongAgent()
 
         self.ball = Ball(b_conf)
         
+        self.paddles = []
         p_conf['x'] = PADDLE_RADIUS * 2
         p_conf['color'] = BLUE
-        self.left_paddle = Paddle(p_conf)
+        self.paddles.append(Paddle(p_conf))
 
         p_conf['x'] = SCREEN_WIDTH - PADDLE_RADIUS * 2
         p_conf['color'] = GREEN
-        self.right_paddle = Paddle(p_conf)
+        self.paddles.append(Paddle(p_conf))
         
         self.obstacles = []
         for _ in range(NUM_OBSTACLES):
             new_o = Obstacle(o_conf)
             self.obstacles.append(new_o)
 
+        self.cur_state = [self.get_state(0), self.get_state(1)]
+        self.nn_model = None
+        if self.nn:
+            self.nn_model = PongAgent()
         self.running = True
+        self.run()
 
     def get_player_moves(self, keys, np):
         # Paddle movement
         if np > 0:
-            self.left_paddle.vy += PADDLE_VEL * (-keys[pygame.K_w] + keys[pygame.K_s])
-            self.left_paddle.vx += PADDLE_VEL * (-keys[pygame.K_a] + keys[pygame.K_d])
+            self.paddles[0].vy += PADDLE_VEL * (-keys[pygame.K_w] + keys[pygame.K_s])
+            self.paddles[0].vx += PADDLE_VEL * (-keys[pygame.K_a] + keys[pygame.K_d])
 
         if np > 1:
-            self.right_paddle.vy += PADDLE_VEL * (-keys[pygame.K_UP] + keys[pygame.K_DOWN])
-            self.right_paddle.vy += PADDLE_VEL * (-keys[pygame.K_LEFT] + keys[pygame.K_RIGHT])
+            self.paddles[1].vy += PADDLE_VEL * (-keys[pygame.K_UP] + keys[pygame.K_DOWN])
+            self.paddles[1].vy += PADDLE_VEL * (-keys[pygame.K_LEFT] + keys[pygame.K_RIGHT])
 
-    def get_state(self, paddle):
+    def get_state(self, rev=False):
         inputs = []
-        
+        paddle, op_paddle = self.paddles[::-1] if rev else self.paddles
         # Ball position and velocity
         inputs.extend([self.ball.x / SCREEN_WIDTH, self.ball.y / SCREEN_HEIGHT])
         inputs.extend([self.ball.vx / BALL_MAX_SPEED, self.ball.vy / BALL_MAX_SPEED])
@@ -122,7 +130,9 @@ class Game:
         # Paddle position and velocity
         inputs.extend([paddle.x / SCREEN_WIDTH, paddle.y / SCREEN_HEIGHT])
         inputs.extend([paddle.vx / PADDLE_VEL, paddle.vy / PADDLE_VEL])
-        
+
+        inputs.extend([op_paddle.x / SCREEN_WIDTH, op_paddle.y / SCREEN_HEIGHT])
+        inputs.extend([op_paddle.vx / PADDLE_VEL, op_paddle.vy / PADDLE_VEL])
         # Encode obstacles positions
         grid_width = SCREEN_WIDTH // MIDGROUND_SPLIT
         grid_height = SCREEN_HEIGHT // MIDGROUND_SPLIT
@@ -137,62 +147,80 @@ class Game:
             obstacle_grid[index] = 1
         
         inputs.extend(obstacle_grid)
-    
-        return inputs
+        
+        return np.reshape(inputs, [1, len(inputs)])
 
     def get_ai_moves(self, players):
         bh = self.ball.y
         vl = self.ball.vy
         fbh = bh + vl**2
         buffer = PADDLE_RADIUS // 8
-        nn_bots = self.config.get('nn', 0)
         follow = lambda x: PADDLE_VEL * (-(x + buffer > fbh) + (x -  buffer < fbh))
         center = lambda x: PADDLE_VEL * min(1, max(-1, (MID_HEIGHT - x)//10))
 
-        def update_paddle(paddle, direction, x_condition, follow_action, center_action):
+        def update_paddle(rev, direction, x_condition, follow_action, center_action):
+            act = [0 for _ in range(5)]
+            paddle = self.paddles[rev]
             if x_condition:
-                paddle.vy += follow_action(paddle.y)
+                fdy = follow_action(paddle.y)
+                act[0] = 1 if fdy > 0 else 0
+                act[1] = 1 if fdy < 0 else 0
+                paddle.vy += fdy
                 if distance(paddle, self.ball) < paddle.r * 3:
-                    paddle.vx += PADDLE_VEL // 2 * direction
+                    adx = PADDLE_VEL // 2 * direction
+                    act[2] = 1 if adx < 0 else 0
+                    act[3] = 1 if adx > 0 else 0
+                    paddle.vx += adx
             else:
-                paddle.vx -= PADDLE_VEL * direction
-                paddle.vy += center_action(paddle.y)
+                ddy = center_action(paddle.y)
+                ddx = PADDLE_VEL * direction
+                act[0] = 1 if ddy > 0 else 0
+                act[1] = 1 if ddy < 0 else 0
+                act[2] = 1 if ddx < 0 else 0
+                act[3] = 1 if ddx > 0 else 0
+                paddle.vx -= ddx
+                paddle.vy += ddy
+            act[-1] = sum(act) == 0
 
-        def nn_play(paddle):
-            model_input = self.get_state(paddle)
-            action = self.nn_model.predict(model_input)
-
-            if action == 0:  # Up
+        def nn_play(rev=False):
+            model_input = self.get_state(rev)
+            action = self.nn_model.act(model_input)
+            paddle = self.paddles[rev]
+            if action[0]:  # Up
                 paddle.vy -= PADDLE_VEL
-            elif action == 1:  # Down
+            elif action[1]:  # Down
                 paddle.vy += PADDLE_VEL
-            elif action == 2:  # Left
+            elif action[2]:  # Left
                 paddle.vx -= PADDLE_VEL
-            elif action == 3:  # Right
+            elif action[3]:  # Right
                 paddle.vx += PADDLE_VEL
+            return action
 
+        bot_actions = []
         if players == 0:
-            if nn_bots == 0: 
-                update_paddle(self.left_paddle, 1, self.ball.vx < 0 and self.ball.x < MID_WIDTH, follow, center)
-                update_paddle(self.right_paddle, -1, self.ball.vx > 0 and self.ball.x > MID_WIDTH, follow, center)
-            elif nn_bots == 1:
-                nn_play(self.left_paddle)
-                update_paddle(self.right_paddle, -1, self.ball.vx > 0 and self.ball.x > MID_WIDTH, follow, center)
+            if self.nn == 0: 
+                bot_actions.append(update_paddle(0, 1, self.ball.vx < 0 and self.ball.x < MID_WIDTH, follow, center))
+                bot_actions.append(update_paddle(1, -1, self.ball.vx > 0 and self.ball.x > MID_WIDTH, follow, center))
+            elif self.nn == 1:
+                bot_actions.append(nn_play(0))
+                bot_actions.append(update_paddle(1, -1, self.ball.vx > 0 and self.ball.x > MID_WIDTH, follow, center))
             else:
-                nn_play(self.left_paddle)
-                nn_play(self.right_paddle)
+                bot_actions.append(nn_play(0))
+                bot_actions.append(nn_play(1))
 
         elif players == 1:
-            if nn_bots > 0:
-                nn_play(self.right_paddle)
+            if self.nn > 0:
+                nn_play(self.paddles[1])
             else:
-                update_paddle(self.right_paddle, -1, self.ball.vx > 0 and self.ball.x > MID_WIDTH, follow, center)
+                update_paddle(self.paddles[1], -1, self.ball.vx > 0 and self.ball.x > MID_WIDTH, follow, center)
+        
+        return bot_actions
     
     def resistance(self):
-        self.left_paddle.vy *= PADDLE_MOVE_DECAY
-        self.left_paddle.vx *= PADDLE_MOVE_DECAY
-        self.right_paddle.vy *= PADDLE_MOVE_DECAY
-        self.right_paddle.vx *= PADDLE_MOVE_DECAY
+        self.paddles[0].vy *= PADDLE_MOVE_DECAY
+        self.paddles[0].vx *= PADDLE_MOVE_DECAY
+        self.paddles[1].vy *= PADDLE_MOVE_DECAY
+        self.paddles[1].vx *= PADDLE_MOVE_DECAY
 
     def toggle_keys(self, keys):
         if keys[pygame.K_COMMA]: self.read_out.inc_fps(1)
@@ -203,36 +231,42 @@ class Game:
         self.resistance()
         self.toggle_keys(keys)
 
-        human_players = self.config['players']
-        if human_players > 0:
-            self.get_player_moves(keys, human_players)
+        if self.players > 0:
+            self.get_player_moves(keys, self.players)
 
-        if human_players < 2: 
-            self.get_ai_moves(human_players)
+        if self.players < 2: 
+            bot_moves = self.get_ai_moves(self.players)
 
-        self.left_paddle.move()
-        self.right_paddle.move()
+        self.paddles[0].move()
+        self.paddles[1].move()
         self.ball.move()
 
         for obs in self.obstacles:
             obs.move()
+        return bot_moves
 
     def check_for_score(self, score, dr):
         p1_score = self.ball.x > SCREEN_WIDTH
         p2_score = self.ball.x < 0
-        
+        p1_win = False
+        p2_win = False
         if p1_score or p2_score:
             if p1_score:
                 score['p1'] += 1
+                self.running = verify(score)
+                p1_win = not self.running
             else:
                 score['p2'] += 1
+                self.running = verify(score)
+                p2_win = not self.running
 
             self.ball.reset()
-            self.left_paddle.reset()
-            self.right_paddle.reset()
+            self.paddles[0].reset()
+            self.paddles[1].reset()
             for ob in self.obstacles:
                 ob.reset()
-        p1_reward = (self.ball.x - MID_WIDTH) / SCREEN_WIDTH + SCORE_REWARD_MULT * (p1_score - p2_score)
+
+        p1_reward = (self.ball.x - MID_WIDTH) / SCREEN_WIDTH + SCORE_REWARD_MULT * (p1_score - p2_score) + WIN_REWARD * (p1_win - p2_win)
         return score, (dr[0] + p1_reward, dr[1] - p1_reward)
     
     def obstacle_collision(self):
@@ -244,8 +278,8 @@ class Game:
                     if obstacle.collides(other):
                         collide(obstacle, other)
 
-    def paddle_collision(self, paddles, rewards):
-        for paddle, rew in zip(paddles, rewards):
+    def paddle_collision(self, rewards):
+        for paddle, rew in zip(self.paddles, rewards):
             if paddle.collides(self.ball):
                 rew += 2
                 paddle.hit = True
@@ -255,27 +289,45 @@ class Game:
         return rewards
 
     def detect_collision(self, score):
-        rewards = [0,0]
-        paddles = [self.left_paddle, self.right_paddle]
-        
         self.obstacle_collision()
-        rewards = self.paddle_collision(paddles, rewards)
+        rewards = self.paddle_collision([0,0])
         return self.check_for_score(score, rewards)
 
     def step(self, score):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-        self.move_objects()
+        moves = self.move_objects()
         score, rewards = self.detect_collision(score)
         self.read_out.update_display(score, rewards)
-        return score, rewards
+        return score, rewards, moves
+
+    def run(self):
+        for game_num in range(self.games):
+            score = {'p1': 0, 'p2': 0}
+            while self.running:
+                score, rewards, actions = self.step(score)
+                
+                if self.train:
+                    next_state = [self.get_state(0), self.get_state(1)]
+                    for x in range(2):
+                        self.nn_model.remember(self.cur_state[x], actions[x], rewards[x], next_state[x], self.running)
+                    self.cur_state = next_state
+            if self.train:
+                self.nn_model.replay()
+        if self.train:
+            self.nn_model.save(MODEL_PATH)
+
+        pygame.quit()
 
 if __name__ == "__main__":
-    conf = {'players': 0}
-    score = {'p1': 0, 'p2': 0}
+    conf = {
+        'players': 0,
+        'nn': 2,
+        'training': True,
+        'num_games': 100
+    }
     g = Game(**conf)
-    while g.running:
-        score, rewards = g.step(score)
-    pygame.quit()
+    
+    
 
